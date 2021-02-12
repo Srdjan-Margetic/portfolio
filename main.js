3759975aeb75b6b72e6f7efc5142f4f6c795fd71
Srđan Margetić <srdjan@stuntcoders.com>
2021-02-13 00:42:44 +0100

    Adjust style for the map on the contact page

--- main.js
+++ main.js
@@ -127,24 +127,5 @@ projectsListPersonal.forEach((el) => {
   });
 });
 
-// Geolocation
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-
-    const myAddressLati = 44.783186;
-    const myAddressLong = 20.4114734;
-    const coords = [myAddressLati, myAddressLong];
-
-      const map = L.map('map').setView(coords, 13);
-
-        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-
-        L.marker(coords).addTo(map)
-            .bindPopup('Here I am! 🖥')
-            .openPopup();
-    }
-  )
-}
+        
+   
